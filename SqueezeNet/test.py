a6ad1c027cec6f8b5fc714317ad117ceab28f997
Pik Ying Wong <>
2024-05-15 22:35:45 +1000

    Update test.py

--- SqueezeNet/test.py
+++ SqueezeNet/test.py
@@ -5,7 +5,7 @@ Created on Mon May  6 12:04:24 2024
 
 @author: annie
 """
-
+import os
 import torch
 from time import time
 import torch.optim as optim
@@ -16,8 +16,11 @@ from torch.utils.data import DataLoader
 from torch.utils.data import random_split
 from squeezeNet_imageClassifier import ImageClassifier
 import matplotlib.pyplot as plt
-
-
+import utils_common
+import time
+import pandas as pd
+import numpy as np
+from squeezeNetGranCam import GradCAM
 
 if __name__ == "__main__":
     transform = transforms.Compose([
@@ -26,52 +29,69 @@ if __name__ == "__main__":
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
     ])
     
-    batch_size = 32
-    learning_rate = 0.001
-    epoch = 30
+    batch_sizes = [32,64,128]
+    learning_rate = [0.0001,0.001,0.01,0.1]
+    epoch = 100
+    all_accuracy = []
+    all_loss = []
+       
+    data_dir = 'data/images/images'
+    classifier = ImageClassifier(data_dir, transform)
+    model = models.squeezenet1_0()
     
-    acc = []
-    epochs = []
+    folders = sorted(os.listdir(data_dir))
+    labels_folder = []
+    for label, folder in enumerate(folders):
+        labels_folder.append(folder)
+        
+    model_filename = 'squeezenet_model_38_0.001_32.pth'
     
-    for i in range(epoch):
-    
-        device = torch.device('mps')
-        model = models.squeezenet1_0(weights=None)
-        checkpoint = torch.load('squeezenet_model_'+str(i+1)+'_'+ str(learning_rate) +'_' + str(batch_size)+'.pth')
-        model.load_state_dict(checkpoint['model_state_dict'])
-        epoch = checkpoint['epoch']
-        loss = checkpoint['loss']
-        #accuracy = checkpoint['accuracy']
-    
-        print("epoch", epoch ,"loss",loss)
-        model.to(device)
-        
-        model.eval()
-        
-        data_dir = 'data/images/images'
-        classifier = ImageClassifier(data_dir, transform)
-    
-        #trainDataloader = DataLoader(classifier.train_data,shuffle=False)
-        validationDataloader = DataLoader(classifier.val_data,shuffle=False)
-        #testDataloader = DataLoader(classifier.test_data,shuffle=False)
-        
-        #trainacc = classifier.evaluate_model(model,trainDataloader)
-        valacc = classifier.evaluate_model(model,validationDataloader,classfication_report=False)
-        #testacc = classifier.evaluate_model(model,testDataloader)
-        
-        #print("acc",testacc ,"valacc",valacc , "trainacc",trainacc)
-        print("valacc",valacc )
-        acc.append(valacc)
-        epochs.append(i)
-        
+    print("==========="+model_filename+"===========")
+   
+    #acc_list,loss_list,loss_images,loss_labels,pred_labels = classifier.report_one_model(model,'model/' +model_filename,'validation')
 
-    # Adding labels and title
-    plt.plot(epochs,acc)
-    plt.xlabel('Validation Accuracy')
-    plt.ylabel('epochs')
-    plt.title('Validation accuracy Chart')
     
-    # Displaying the plot
-    plt.show()
+    # for i, images in enumerate(loss_images):
+
+    #     gradcam = GradCAM(model)
+    #     gradcam.register_hooks()
+        
+    #     heatmap_true = gradcam.generate(images, target_class=loss_labels[i])
+    #     heatmap_pred = gradcam.generate(images, target_class=pred_labels[i])
+        
+    #     gradcam.remove_hooks()
+        
+    #     if loss_labels[i][0].cpu().numpy()==5 :
             
+    #         true_label_index = loss_labels[i][0].cpu().numpy()
+    #         pred_label_index = pred_labels[i][0].cpu().numpy()
+            
+    #         true_label  = labels_folder[true_label_index]
+    #         pred_label = labels_folder[pred_label_index]
+            
+    #         gradcam.plotImages(images.squeeze().cpu().numpy(),heatmap_true,"true label " + true_label)
+    #         gradcam.plotImages(images.squeeze().cpu().numpy(),heatmap_pred,"predicted Label " + pred_label)
+    
+    # for batch_size in batch_sizes:
+    #     df_acc = pd.DataFrame()
+    #     df_loss = pd.DataFrame()
+        
+    #     for lr in learning_rate:
+    #         acc , loss = classifier.report(model,epoch,lr,batch_size,'validation')
+    #         df_acc[str(lr)] = pd.Series(acc)
+    #         df_loss[str(lr)]= pd.Series(loss)
+        
+    #     df_acc.to_csv('result/data_acc_batch'+str(batch_size)+'.csv', index=False)
+    #     df_loss.to_csv('result/data_loss_batch'+str(batch_size)+'.csv', index=False)
+      
+    
+    for batch_size in batch_sizes:  
+        df_acc = pd.read_csv('result/data_acc_batch'+str(batch_size)+'.csv', header=0)
+        df_loss= pd.read_csv('result/data_loss_batch'+str(batch_size)+'.csv', header=0)
+        classifier.plot_cureve_csv(df_loss,df_acc, batch_size)
+        for lr in learning_rate:
+            if str(lr) in df_acc.columns.tolist():
+                print("Accuracy : batch_sizes:",batch_size, ",learning rate: ",lr ,": " , df_acc[str(lr)].max(), "," ,  df_acc[str(lr)].idxmax()+1)
+                print("Loss : batch_sizes:",batch_size, ",learning rate: ",lr ,": " , df_loss[str(lr)].min(), "," ,  df_loss[str(lr)].idxmin()+1)
+    
     
