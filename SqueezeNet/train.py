a9dba63b043a7c2ab0dcce32ccbaef7383e33d3d
Pik Ying Wong <>
2024-05-15 22:35:47 +1000

    Update train.py

--- SqueezeNet/train.py
+++ SqueezeNet/train.py
@@ -9,7 +9,7 @@ import torch
 from time import time
 import torch.optim as optim
 from torchvision import datasets ,transforms ,models
-from torchvision.models import SqueezeNet
+#from torchvision.models import SqueezeNet
 from sklearn.metrics import accuracy_score
 from torch.utils.data import DataLoader 
 from torch.utils.data import random_split
@@ -23,15 +23,18 @@ if __name__ == "__main__":
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
     ])
-    data_dir = 'data/images/images'
-    classifier = ImageClassifier(data_dir, transform)
-    device = torch.device('mps')
-    model = models.squeezenet1_0(weights=None)
-    model.to(device)
-    epochs = 30
-    batch_size = 32
-    learning_rate = 0.001
     
+    data_dir = '/Users/annie/Documents/GitHub/STAT7007/SqueezeNet/data/images/images'
+    squeezeNetModel = models.squeezenet1_0(weights=None)
+    classifier = ImageClassifier(data_dir, transform,model=squeezeNetModel)
+   
     
-    acc_tr, epoch_losses = classifier.training_model(model, classifier.train_data, epochs=epochs, batch_size=batch_size, 
-                                                     learning_rate=learning_rate, device=device)
+    epochs = 100
+    batch_size = 128
+    learning_rate = [0.0001,0.001]
+    
+    for lr in learning_rate :
+        acc_tr, epoch_losses = classifier.training_model(epochs=epochs, batch_size=batch_size, 
+                                                         learning_rate=lr)
+
+
